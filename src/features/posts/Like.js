31df1942ce681818f3ec9bb0a1ed1fcf16e34a4b
Villy Siu <villysiu@gmail.com>
2023-01-06 15:42:06 -0800

    post like feature

---
+++ src/features/posts/Like.js
@@ -0,0 +1,8 @@
+import { Heart, HeartFill } from 'react-bootstrap-icons'
+
+const Like = () => {
+    return (
+        <HeartFill />
+    )
+}
+export default Like
--- src/features/posts/Post.js
+++ src/features/posts/Post.js
@@ -1,13 +1,22 @@
 import { Card } from 'react-bootstrap';
+import {  ThreeDots } from "react-bootstrap-icons"
+import Like from './Like';
+import LikeCounter from './LikeCounter';
 const Post =(params)=>{
     console.log("in post")
-    const {url, desc, owner_name} = params
+    const {url, desc, owner_name, liked_users} = params
     return (
        
-            <Card style={{ width: '18rem' }}>
-                <Card.Title>{owner_name}</Card.Title>
-                <Card.Img style={{size: 'cover'}} variant="top" src={`http://localhost:3000/${url}`} />
-                <Card.Text> {desc} </Card.Text>
+            <Card id="card">
+                <Card.Title className="card_header">
+                    <span>{owner_name} </span>
+                    <span><ThreeDots style = {{transform: 'rotate(90deg)' }} /> </span>
+                </Card.Title>
+
+                <Card.Img className="card_img" variant="top" src={`http://localhost:3000/${url}`} />
+                
+                <Card.Text className="card_likes"><Like /> {liked_users.length} likes</Card.Text>
+                <Card.Text className="card_desc"> {desc} </Card.Text>
             </Card> 
  
           
